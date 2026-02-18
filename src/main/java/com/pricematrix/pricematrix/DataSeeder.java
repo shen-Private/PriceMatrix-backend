45850a8c93cc1e93add600b375a61ebf9658f99c
SHEN TUNG <vcd52088@gmail.com>
2026-02-18 22:31:39 +0900

    Fix DataSeeder to use setters

--- src/main/java/com/pricematrix/pricematrix/DataSeeder.java
+++ src/main/java/com/pricematrix/pricematrix/DataSeeder.java
@@ -26,30 +26,40 @@ public class DataSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        // 已有資料就不重複插入
         if (customerRepository.count() > 0) return;
 
-        // 分類
-        Category c1 = categoryRepository.save(new Category(null, "飲料"));
-        Category c2 = categoryRepository.save(new Category(null, "零食"));
-        Category c3 = categoryRepository.save(new Category(null, "日用品"));
+        Category c1 = new Category(); c1.setName("飲料");
+        Category c2 = new Category(); c2.setName("零食");
+        Category c3 = new Category(); c3.setName("日用品");
+        categoryRepository.save(c1);
+        categoryRepository.save(c2);
+        categoryRepository.save(c3);
 
-        // 客戶
-        Customer cu1 = customerRepository.save(new Customer(null, "張三"));
-        Customer cu2 = customerRepository.save(new Customer(null, "李四"));
-        Customer cu3 = customerRepository.save(new Customer(null, "王五"));
+        Customer cu1 = new Customer(); cu1.setName("張三");
+        Customer cu2 = new Customer(); cu2.setName("李四");
+        Customer cu3 = new Customer(); cu3.setName("王五");
+        customerRepository.save(cu1);
+        customerRepository.save(cu2);
+        customerRepository.save(cu3);
 
-        // 商品
-        Product p1 = productRepository.save(new Product(null, "可樂", c1));
-        Product p2 = productRepository.save(new Product(null, "綠茶", c1));
-        Product p3 = productRepository.save(new Product(null, "洋芋片", c2));
-        Product p4 = productRepository.save(new Product(null, "衛生紙", c3));
+        Product p1 = new Product(); p1.setName("可樂"); p1.setCategory(c1);
+        Product p2 = new Product(); p2.setName("綠茶"); p2.setCategory(c1);
+        Product p3 = new Product(); p3.setName("洋芋片"); p3.setCategory(c2);
+        Product p4 = new Product(); p4.setName("衛生紙"); p4.setCategory(c3);
+        productRepository.save(p1);
+        productRepository.save(p2);
+        productRepository.save(p3);
+        productRepository.save(p4);
 
-        // 折扣
-        discountRepository.save(new Discount(null, cu1, p1, new BigDecimal("0.85")));
-        discountRepository.save(new Discount(null, cu1, p2, new BigDecimal("0.90")));
-        discountRepository.save(new Discount(null, cu2, p1, new BigDecimal("0.80")));
-        discountRepository.save(new Discount(null, cu2, p3, new BigDecimal("0.75")));
-        discountRepository.save(new Discount(null, cu3, p4, new BigDecimal("0.95")));
+        Discount d1 = new Discount(); d1.setCustomer(cu1); d1.setProduct(p1); d1.setDiscountRatio(new BigDecimal("0.85"));
+        Discount d2 = new Discount(); d2.setCustomer(cu1); d2.setProduct(p2); d2.setDiscountRatio(new BigDecimal("0.90"));
+        Discount d3 = new Discount(); d3.setCustomer(cu2); d3.setProduct(p1); d3.setDiscountRatio(new BigDecimal("0.80"));
+        Discount d4 = new Discount(); d4.setCustomer(cu2); d4.setProduct(p3); d4.setDiscountRatio(new BigDecimal("0.75"));
+        Discount d5 = new Discount(); d5.setCustomer(cu3); d5.setProduct(p4); d5.setDiscountRatio(new BigDecimal("0.95"));
+        discountRepository.save(d1);
+        discountRepository.save(d2);
+        discountRepository.save(d3);
+        discountRepository.save(d4);
+        discountRepository.save(d5);
     }
 }
